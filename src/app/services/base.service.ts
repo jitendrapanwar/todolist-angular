92833bddc5bac5780f4cf9a228e1602efee4e037
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-03 17:36:19 +0200

    removed unused methods/calls.

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -21,24 +21,11 @@ export class BaseService {
 
   constructor(private http:HttpClient) { }
  
-  getPostById(id) {
-    return this.http.get<Post>(`${API_URL}${REQUEST.GET_POSTS}/${id}`)
-  }
-
   getPosts() {
     return this.http.get<Post>(`${API_URL}${REQUEST.GET_POSTS}`)
   }
 
-  getComments() {
-    return this.http.get<Comment>(`${API_URL}${REQUEST.GET_COMMENTS}`)  
-  }
-  
   getCommentByPostId(id) {
     return this.http.get<Comment>(`${API_URL}${REQUEST.GET_COMMENTS}?postId=${id}`)  
   }
-
-  getProfile() {
-    return this.http.get(`${API_URL}${REQUEST.GET_PROFILE}`)
-  }
-
 }
