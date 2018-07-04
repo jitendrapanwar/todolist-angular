488f354bba5ed80bbac6716226ad25449988e0bd
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-04 16:47:19 +0200

    implemented function to show all posts and their respective comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { BaseService, Post, Comment } from './services/base.service';
-import { takeUntil } from "rxjs/operators";
-import { Subject } from 'rxjs';
-
+import { BaseService } from './services/base.service';
+import { takeUntil, catchError } from "rxjs/operators";
+import { Observable, Subject, forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  posts=[];
-  comments=[];
-  errorMessage:string;
+export class AppComponent implements OnInit {
+  posts: Array<any>;
+  errorMessage: string;
+  posts$: Observable<any>;
+  comments$: Observable<any>;
 
   private destroy$: Subject<null> = new Subject<null>();
 
   constructor(private bs:BaseService) {}
 
   ngOnInit(): void {
-    this.bs.getPosts().pipe(
-      takeUntil(this.destroy$)
-    ).subscribe(data => {
-      this.posts = data as any;
-    }, err => this.errorMessage = err.message)
-  } 
-
-  getComments(post:Post) {
-    const { id } = post;
-   
-    this.bs.getCommentByPostId(id).pipe(
+    
+    this.posts$ = this.bs.getPosts().pipe(
       takeUntil(this.destroy$),
-    ).subscribe((comment:Comment) => {
-      this.comments = comment as any;
-    }, err => this.errorMessage = err.message);
+    );
+
+    this.comments$ = this.bs.getComments().pipe(
+      takeUntil(this.destroy$)
+    );
+
+    forkJoin([this.posts$, this.comments$]).pipe(
+      takeUntil(this.destroy$),
+    ).subscribe((results) => {
+      this.posts = this.buildPostsData(results)
+    }, (err) => this.errorMessage = err.message);
+  }
+  
+  buildPostsData(results) {
+    const [posts, comments] = results;
+    
+    return posts.map(({...post}) => {
+      return comments.reduce((accumulator, comment) => {
+        accumulator = post;
+        accumulator.comments = accumulator.comments || []; 
+        if(accumulator.id === comment.postId) {
+          accumulator.comments.push(comment);
+        }
+        return accumulator;
+      },[]);
+    })
   }
 
   ngOnDestroy(): void {
