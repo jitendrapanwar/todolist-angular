9d51d5a6534f68529b84ba9b2413a1e8b7956e65
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-09 09:00:11 +0200

    linting issue fixed

--- src/app/app.endpoints.ts
+++ src/app/app.endpoints.ts
@@ -1,7 +1,7 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = 'http://localhost:3000';
 
 export const REQUEST = {
     GET_POSTS : '/posts',
     GET_COMMENTS : '/comments',
     GET_PROFILE : '/profile',
-}
+};
