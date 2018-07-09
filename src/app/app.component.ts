82ed5a55d69cc3017cf8f0199b1e03c3c0c277e3
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-09 09:04:16 +0200

    committed Davide's code implementation regarding fetching data by postID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseService } from './services/base.service';
-import { takeUntil, catchError } from "rxjs/operators";
-import { Observable, Subject, forkJoin} from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BaseService} from './services/base.service';
+import {flatMap} from 'rxjs/operators';
+import {from, Subscription} from 'rxjs';
+import {tap} from 'rxjs/internal/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  posts: Array<any>;
+export class AppComponent implements OnInit, OnDestroy {
+  posts: Array<any> = [];
   errorMessage: string;
-  posts$: Observable<any>;
-  comments$: Observable<any>;
-
-  private destroy$: Subject<null> = new Subject<null>();
-
-  constructor(private bs:BaseService) {}
+  postsSubscription: Subscription;
+  combind$;
+  constructor(private bs: BaseService) {}
 
   ngOnInit(): void {
-    
-    this.posts$ = this.bs.getPosts().pipe(
-      takeUntil(this.destroy$),
-    );
-
-    this.comments$ = this.bs.getComments().pipe(
-      takeUntil(this.destroy$)
-    );
-
-    forkJoin([this.posts$, this.comments$]).pipe(
-      takeUntil(this.destroy$),
-    ).subscribe((results) => {
-      this.posts = this.buildPostsData(results)
-    }, (err) => this.errorMessage = err.message);
-  }
-  
-  buildPostsData(results) {
-    const [posts, comments] = results;
-    
-    return posts.map(({...post}) => {
-      return comments.reduce((accumulator, comment) => {
-        accumulator = post;
-        accumulator.comments = accumulator.comments || []; 
-        if(accumulator.id === comment.postId) {
-          accumulator.comments.push(comment);
-        }
-        return accumulator;
-      },[]);
-    })
+    this.bs.getPosts().pipe(
+      flatMap((posts) => {
+        return from(posts);
+     }),
+      flatMap((post) => {
+        return this.bs.getCommentByPostId(post.id).pipe(
+          tap((comments) => {
+            post.comments = comments;
+            this.posts.push(post);
+          })
+        );
+      })
+    ).subscribe((posts) => {
+      console.log(posts);
+    });
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.unsubscribe();
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
   }
 }
