240520ff9fb3775190435660eac7876efc7d2624
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-09 09:05:46 +0200

    committed Davide's code implementation corrected Post interface

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { REQUEST, API_URL } from '../app.endpoints';
+import { Observable } from 'rxjs';
 
 export interface Post {
-  "id": string,
-  "title": string,
-  "author": string
+  id: string;
+  title: string;
+  author: string;
+  comments: Comment[];
 }
 
 export interface Comment {
-  "id": string,
-  "body": string,
-  "postId": string
+  id: string;
+  body: string;
+  postId: string;
 }
 
 @Injectable({
@@ -19,17 +21,17 @@ export interface Comment {
 })
 export class BaseService {
 
-  constructor(private http:HttpClient) { }
- 
-  getPosts() {
-    return this.http.get<Post>(`${API_URL}${REQUEST.GET_POSTS}`)
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${API_URL}${REQUEST.GET_POSTS}`);
   }
 
-  getComments() {
-    return this.http.get<Comment>(`${API_URL}${REQUEST.GET_COMMENTS}`)  
+  getComments(): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${API_URL}${REQUEST.GET_COMMENTS}`);
   }
 
-  getCommentByPostId(id) {
-    return this.http.get<Comment>(`${API_URL}${REQUEST.GET_COMMENTS}?postId=${id}`)  
+  getCommentByPostId(id): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${API_URL}${REQUEST.GET_COMMENTS}?postId=${id}`);
   }
 }
