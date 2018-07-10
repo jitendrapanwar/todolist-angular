ab206e5e26a97eac9f05328978ec653674579006
jitendra panwar <jitendra.panwar@hcl.com>
2018-07-10 09:00:20 +0200

    improved code structure.

--- src/app/services/base.service.spec.ts
+++ src/app/services/base.service.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed, inject, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { API_URL } from '../app.endpoints';
-import { BaseService, Post, Comment } from './base.service';
-
-
-let injector: TestBed;
-let service: BaseService;
-let httpMock: HttpTestingController;
+import { BaseService, Comment } from './base.service';
 
 describe('BaseService', () => {
+  let injector: TestBed;
+  let service: BaseService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,80 +27,66 @@ describe('BaseService', () => {
     expect(bs).toBeTruthy();
   }));
 
-});
+  describe('#getPosts', () => {
+    it('should return list of Observable<Post[]>', () => {
+      const mockPosts: any[] = [
+        {
+          'id': '1',
+          'title': 'post-1',
+          'author': 'typicode'
+        }
+      ];
 
-describe('#getPosts', () => {
-  it('should return an Observable<Post[]>', () => {
-    const fakePosts: Post[] = [
-      {
-        'id': '1',
-        'title': 'post-1',
-        'author': 'typicode',
-        'comments': [
-          {
-            'id': '1',
-            'body': 'post-1 comment-1',
-            'postId': '1'
-          }
-        ]
-      }
-    ];
+      service.getPosts().subscribe(posts => {
+        expect(posts.length).toBe(mockPosts.length);
+      });
 
-    service.getPosts().subscribe(posts => {
-      expect(posts.length).toBe(1);
-      expect(posts).toEqual(fakePosts);
+      const req = httpMock.expectOne(`${API_URL}/posts`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockPosts);
     });
-
-    const req = httpMock.expectOne(`${API_URL}/posts`);
-    expect(req.request.method).toBe('GET');
-    req.flush(fakePosts);
   });
-});
 
-describe('#getComments', () => {
-  it('should return an Observable<Comment[]>', () => {
-    const fakeComments: Comment[] = [
-      {
-        'id': '1',
-        'body': 'post-1 comment-1',
-        'postId': '1'
-      },
-      {
-        'id': '2',
-        'body': 'post-2 comment-1',
-        'postId': '2'
-      },
-    ];
+  describe('#getComments', () => {
+    it('should return list of Observable<Comment[]>', () => {
+      const fakeComments: Comment[] = [
+        {
+          'id': '1',
+          'body': 'post-1 comment-1',
+          'postId': '1'
+        },
+        {
+          'id': '2',
+          'body': 'post-2 comment-1',
+          'postId': '2'
+        },
+      ];
 
-    service.getComments().subscribe(comment => {
-      expect(comment.length).toBe(2);
+      service.getComments().subscribe(comment => {
+        expect(comment.length).toBe(2);
+      });
+
+      const req = httpMock.expectOne(`${API_URL}/comments`);
+      expect(req.request.method).toBe('GET');
+      req.flush(fakeComments);
     });
-
-    const req = httpMock.expectOne(`${API_URL}/comments`);
-    expect(req.request.method).toBe('GET');
-    req.flush(fakeComments);
   });
-});
 
+  describe('#getCommentByPostId', () => {
+    it('should return an Observable<Comment[] by Post ID>', () => {
+      const mockPosts: any = {
+          'id': '1',
+          'title': 'post-1',
+          'author': 'typicode'
+        };
 
-describe('#getCommentByPostId', () => {
-  it('should return an Observable<Comment[] by Post ID>', () => {
-    const fakeComments: Comment[] = [
-      {
-        'id': '1',
-        'body': 'post-1 comment-1',
-        'postId': '1'
-      }
-    ];
+      service.getCommentByPostId(mockPosts.id).subscribe(response => {
+        expect(response).toBe(mockPosts.id);
+      });
 
-    const postId = '1';
-
-    service.getCommentByPostId(postId).subscribe(comment => {
-      expect(comment.length).toBe(1);
+      const req = httpMock.expectOne(`${API_URL}/comments?postId=${mockPosts.id}`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockPosts.id);
     });
-
-    const req = httpMock.expectOne(`${API_URL}/comments?postId=${postId}`);
-    expect(req.request.method).toBe('GET');
-    req.flush(fakeComments);
   });
 });
